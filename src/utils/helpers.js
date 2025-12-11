2d88e911da3f851c2c119e39d23a233c17b2ccf9
Rodney Austria <mrtangkad59@gmail.com>
2025-12-11 17:03:45 +0800

    helpers, for timestamp and random username generation

---
+++ src/utils/helpers.js
@@ -0,0 +1,19 @@
+// src/utils/helpers.js
+
+/**
+ * Formats a timestamp into a readable 12-hour time format (e.g., "10:30 PM")
+ */
+export const formatTime = (timestamp) => {
+  if (!timestamp) return '';
+  return new Date(timestamp).toLocaleTimeString([], { 
+    hour: '2-digit', 
+    minute: '2-digit' 
+  });
+};
+
+/**
+ * Generates a random username for testing purposes
+ */
+export const generateRandomUsername = () => {
+  return "Candidate_" + Math.floor(Math.random() * 10000);
+};
